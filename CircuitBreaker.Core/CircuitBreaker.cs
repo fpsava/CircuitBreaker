88619ca4b8c17cd6a939ba701ce0cfadfae032b3
Francesco Pichierri <fpsava@gmail.com>
2015-05-21 08:57:11 +0200

    ReplyScenario

---
+++ CircuitBreaker.Core/CircuitBreaker.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+
+namespace CircuitBreakerPattern.Core
+{
+    public class CircuitBreaker
+    {
+        private readonly String _name;
+        private readonly Action<Exception> _tripAction;
+        private readonly Int32 _maxTimesToRetry;
+        private readonly TimeSpan _delayBetweenRetries;
+        private readonly Timer _timer;
+        private Exception _lastException;
+        private Int64 _failureCount;
+
+        public CircuitBreaker(String name, Action<Exception> tripAction, Int32 maxTimesToRetry, TimeSpan delayBetweenRetries)
+        {
+            _name = name;
+            _tripAction = tripAction;
+            _maxTimesToRetry = maxTimesToRetry;
+            _delayBetweenRetries = delayBetweenRetries;
+
+            _timer = new Timer(CircuitBreakerTripped, null, Timeout.Infinite, (Int32)delayBetweenRetries.TotalMilliseconds);
+        }
+
+        private void CircuitBreakerTripped(Object state)
+        {
+            Console.WriteLine("Check to see if we need to trip the circuit breaker. Retry:{0}", _failureCount);
+            if (Interlocked.Increment(ref _failureCount) > _maxTimesToRetry)
+            {
+                Console.WriteLine("The circuit breaker for {0} is now tripped. Calling specified action", _name);
+                _tripAction(_lastException);
+                return;
+            }
+            _timer.Change(_delayBetweenRetries, TimeSpan.FromMilliseconds(-1));
+        }
+
+        public void Trip(Exception exception)
+        {
+            _lastException = exception;
+            var newValue = Interlocked.Increment(ref _failureCount);
+
+            if (newValue == 1)
+            {
+                // Start the retry timer.
+                _timer.Change(_delayBetweenRetries, TimeSpan.FromMilliseconds(-1));
+
+                // Log that the circuit breaker is triggered.
+                Console.WriteLine("The circuit breaker for {0} is now in the armed state", _name);
+            }
+        }
+    }
+}
---
+++ CircuitBreaker.Tests/CircuitBreakerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CircuitBreakerPattern.Core;
+using NUnit.Framework;
+
+namespace CircuitBreakerPattern.Tests
+{
+    /// <summary>
+    /// http://particular.net/blog/protect-your-software-with-the-circuit-breaker-design-pattern
+    /// </summary>
+    [TestFixture]
+    public class CircuitBreakerTests
+    {
+        [Test]
+        public void When_the_circuit_breaker_is_tripped_the_trip_action_is_called_after_reaching_max_threshold()
+        {
+            var circuitBreakerTripActionCalled = false;
+            var connectionException = new Exception("Something bad happened.");
+
+            var circuitBreaker = new CircuitBreaker("CheckServiceConnection", exception =>
+            {
+                Console.WriteLine("Circuit breaker tripped - fail fast");
+                circuitBreakerTripActionCalled = true;
+                
+                //You would normally fail fast here in the action to faciliate the process shutdown by calling"
+                Console.WriteLine("do stuff!!!");
+            }, 3, TimeSpan.FromSeconds(1));
+
+            circuitBreaker.Trip(connectionException);
+            System.Threading.Thread.Sleep(5000);
+            Assert.IsTrue(circuitBreakerTripActionCalled);
+        }
+    }
+}
